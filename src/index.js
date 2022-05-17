e0eb6f17c5a14c0d3e211cc0e3c6800b8c14a650
Phuc <phucdh@datxanh.com.vn>
2022-05-17 16:01:38 +0700

    add 17/05/2022

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/global.scss';
+import App from './views/App';
+import reportWebVitals from './reportWebVitals';
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
